be5612829bfc258140be80526d3cd3f28647370d
David Balogh <davidbalogh@davids-mbp.home>
2022-10-11 20:27:58 +0300

    Routes

--- David_Balogh_Lab1/Program.cs
+++ David_Balogh_Lab1/Program.cs
@@ -24,5 +24,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "greeting",
+    pattern: "{controller=Greet}/{action=Index}");
+
+
 app.Run();
 
